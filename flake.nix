e7221e82ae34d4aef09210634a64f5de9428b471
Jesper Korsen <Jesper.Korsen@gmail.com>
2026-04-23 12:24:33 +0300

    Add NixOS dev flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.clang-tools
+          pkgs.clang
+          pkgs.cmake
+          pkgs.qemu
+        ];
+      };
+    };
+}
