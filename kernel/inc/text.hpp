d8dae25010aab237fcbb2b2c8b4b4a0d622eea2f
Jesper Korsen <Jesper.Korsen@gmail.com>
2026-05-14 09:50:47 +0300

    WIP seperate cpp file for text

---
+++ kernel/inc/text.hpp
@@ -0,0 +1,9 @@
+// #pragma once
+
+// extern "C" {
+//     extern const std::uint8_t font_u_vga16_start[];
+//     extern const std::uint8_t font_u_vga16_end[];
+// }
+
+// const unsigned char* data;
+// std::size_t size;
